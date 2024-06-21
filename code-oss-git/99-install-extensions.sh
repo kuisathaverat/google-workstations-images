0bc0977b1a706d9bfa3c908f037770b4c84c1740
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2024-06-21 16:18:31 +0200

    feta: install extensions

---
+++ code-oss-git/99-install-extensions.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+/opt/code-oss/bin/codeoss-cloudworkstations --install-extension ms-python.python \
+  --install-extension Github.vscode-pull-request-github \
+  --install-extension golang.go \
+  --install-extension editorconfig.editorconfig
